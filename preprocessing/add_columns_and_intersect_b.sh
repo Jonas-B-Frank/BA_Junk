fd9cc0b7ac33940b87ffcb38f0958bbc0a2ff33a
JonasBFrank <jonas1.frank@uni-a.de>
2023-09-07 06:28:28 +0000

    changed_files

--- preprocessing/add_columns_and_intersect_b.sh
+++ preprocessing/add_columns_and_intersect_b.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-snvs_with_segments=../snvs_with_segments/$1
+snvs_with_segments=../b/$1
 onek1k=../onek1k/onek1k_subset_columns.tsv
 dest_extra_columns=../snvs_with_segments_extra_columns
 dest_joined=../joined_mmml_onek1k
@@ -11,8 +11,8 @@ for file in $snvs_with_segments/*; do
     echo "Add columns on PID ${pid}"
     python3 add_columns.py -f $file -o ${dest_extra_columns}/extra_columns_${pid}.vcf.gz
     
-    echo "Intersect on PID ${pid}"
-    intersectBed -wa -wb -a ${dest_extra_columns}/extra_columns_${pid}.vcf.gz -b $onek1k | gzip > ${dest_joined}/mmmml_onek1k_${pid}.vcf.gz
+    # echo "Intersect on PID ${pid}"
+    # intersectBed -wa -wb -a ${dest_extra_columns}/extra_columns_${pid}.vcf.gz -b $onek1k | gzip > ${dest_joined}/mmmml_onek1k_${pid}.vcf.gz
 
     echo "Done for PID ${pid}"
 done
